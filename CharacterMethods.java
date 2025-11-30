1e977e1e8b67b0e27f735fd9b9edc030a270036c
Krishna Gupta <gkrish3108@gmail.com>
2025-11-30 12:21:00 +0530

    Create CharacterMethods.java

---
+++ CharacterMethods.java
@@ -0,0 +1,23 @@
+package Javas;
+
+public class CharacterMethods {
+    
+    public static void main(String[] args){
+
+        char s = ' ';
+        char c='a';
+        int i ='0'; 
+        System.out.println(Character.isLowerCase(c));
+
+        System.out.println(Character.isUpperCase(c));
+
+        System.out.println(Character.toUpperCase(c));
+
+        System.out.println(Character.toLowerCase(c));
+
+        System.out.println(Character.isDigit(i));
+
+        System.out.println(Character.isSpaceChar(s));
+
+    }
+}

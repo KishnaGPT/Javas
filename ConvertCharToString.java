e1bac334e6b9505cca45073553efbe3f2b398a8b
Krishna Gupta <gkrish3108@gmail.com>
2025-12-04 08:05:33 +0530

    Create ConvertCharToString.java

---
+++ ConvertCharToString.java
@@ -0,0 +1,12 @@
+public class ConvertCharToString {
+
+    public static void main(String[] args){
+
+        char[] c1 = {'A','V','E','N','G','E','R'};
+
+        String sc1 = new String(c1);
+
+        System.out.println(sc1);
+
+    }
+}

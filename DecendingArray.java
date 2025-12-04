bc7fcb2f97dff7c794cc1e21c7f5d15c4b08ff96
Krishna Gupta <gkrish3108@gmail.com>
2025-12-04 08:06:01 +0530

    Create DecendingArray.java

---
+++ DecendingArray.java
@@ -0,0 +1,18 @@
+package java;
+public class DecendingArray {
+    
+    public static void main(String[] args){
+
+        int [] arr = {10,90,100,50,40,30,90};
+
+        sortArray(arr);
+
+        System.out.println("Sorted Array in Descending Order is: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+    }
+
+    pub
+}

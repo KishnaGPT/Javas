b780ef9950a3daab32a2c1893c6ee4fb0ed5c521
Krishna Gupta <gkrish3108@gmail.com>
2026-01-10 09:56:39 +0530

    Create Opera.java

---
+++ Opera.java
@@ -0,0 +1,13 @@
+import calculations.*;
+
+public class Opera {
+    
+    public static void main(String[] args){
+
+        Add a1 = new Add(10,20);
+        // a1.doAdd();
+
+        Sub s1 = new Sub(30, 15);
+
+    }
+}
